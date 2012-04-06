23e09b2405acd42b26aa84fd3eb82a25ff2b9fea
Dominik Kundel <dominik.kundel@me.com>
2012-04-06 18:16:48 +0200

    Still bug when turning left

--- panorama.js
+++ panorama.js
@@ -7,7 +7,6 @@ var pos_y = 0;
 var play_arg = 0;
 
 var img = new Image();
-img.onload = draw;
 img.src="example.jpg";
 
 var draw = function(){
@@ -16,12 +15,23 @@ var draw = function(){
 	var ctx = c.getContext("2d");
 	ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height)
 
-	if(pos_x >= img.width){
-		pos_x = pos_x - img.width;
+	if((Math.abs(pos_x))>=img.width){
+				pos_x += img.width;
 	}
+
+	if((Math.abs(pos_x))<=img.width){
+		pos_x -=img.width;
+	}
+
 	if(scale_both!=1){
 		ctx.scale(scale_both, scale_both);
 		scale_both = 1;
+	}
+	if(2*((Math.abs(pos_x)+ctx.canvas.width))>img.width){
+		ctx.drawImage(img, (pos_x+img.width), pos_y);
+	}
+	if(2*((Math.abs(pos_x)-ctx.canvas.width))<img.width){
+		ctx.drawImage(img, (pos_x-img.width), pos_y);
 	}
 	ctx.drawImage(img, pos_x, pos_y);
 };
@@ -29,11 +39,21 @@ var draw = function(){
 var move_pic_right = function(pix, i){
 	setTimeout(function(){
 		if(i<=pix){
-			pos_x -= 1;
+			pos_x -= 5;
 			draw();
-			self.move_pic_right(pix, i+1);
+			self.move_pic_right(pix, i+5);
 		}
 	},10);
+}
+
+var move_pic_left = function(pix, i){
+	setTimeout(function(){
+		if(i<=pix){
+			pos_x += 5;
+			draw();
+			self.move_pic_left(pix, i+5)
+		}
+	}, 10);
 }
 
 var play_right = function(){
@@ -44,10 +64,10 @@ var play_right = function(){
 var go_right = function(){
 	if(play_arg!=1){
 		setTimeout(function(){
-			pos_x -= 1;
+			pos_x -= 4;
 			draw();
 			self.go_right();
-		},10);
+		},20);
 	}
 }
 
